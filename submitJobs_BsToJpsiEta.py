b8674841996920df96751d3809006a4734056d70
Alibordi <alibordi.muhammad@cern.ch>
2024-10-02 21:25:08 +0200

    particle ID and name changed

---
+++ submitJobs_BsToJpsiEta.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+import os
+
+from python.utilityFunctions import *
+###################################################
+#BsToJpsiEta : Jpsi -> mu+ mu- , Eta -> gamma gamma
+###################################################
+
+CMSSW_BASE = os.environ.get("CMSSW_BASE")
+genFragmentsDirectory = "Configuration/GenProduction/python/GenFragments/"
+generator_fragments= [
+                    "TSG-Run3Summer22EEGS-000-443221.py"
+                    #"TSG-Run3Summer22EEGS-00030.py"
+                    #"TSG-Run3Summer22EEGS-00029.py"
+                    #"TSG-Run3Summer22EEGS-00024.py"
+]
+
+
+#Job steering parameters
+era = "Run2022"
+workAreaName = "tasks_BsToJpsiEta_MCTunesRun3ECM13p6TeV"
+eventsPerJob = 5000
+numberOfJobs = 200
+outLFNDirBase = "/store/group/dpg_trigger/comm_trigger/L1Trigger/OMTF/TrackingVertexing/"
+storage_element="T2_CH_CERN"
+outputDatasetTag = "BsToJpsiEta_14_0_15_patch1_02_10_2024"
+withPileUp = False
+withReco = True
+runLocal = False
+
+
+#########################################
+for aGenFragment in generator_fragments:
+
+    requestName = aGenFragment.split("00025")[0].rstrip("-")
+    requestName+="_"+era+"_"+outputDatasetTag
+    process = runCMSDriver(era, withPileUp, withReco, genFragmentsDirectory+aGenFragment)
+    dumpProcess(process, "PSet.py")
+
+    prepareCrabCfg(workAreaName, eventsPerJob, numberOfJobs,
+                  outLFNDirBase, storage_element, 
+                  requestName, outputDatasetTag)
+
+    if not runLocal:
+        os.system("crab submit -c crabTmp.py")
+        os.system("rm -f PSet.py* crabTmp.py*")      
+#######################################################
---
+++ submitJobs_BsToPhiGamma.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import os
+
+from python.utilityFunctions import *
+###################################################
+#BsToPhiGamma : phi -> mu+ mu- ,  gamma
+###################################################
+CMSSW_BASE = os.environ.get("CMSSW_BASE")
+genFragmentsDirectory = "Configuration/GenProduction/python/GenFragments/"
+generator_fragments= [
+                    "TSG-Run3Summer22EEGS-000-333-22.py"
+                    #"TSG-Run3Summer22EEGS-00030.py"
+                    #"TSG-Run3Summer22EEGS-00029.py"
+                    #"TSG-Run3Summer22EEGS-00024.py"
+]
+
+
+#Job steering parameters
+era = "Run2022"
+workAreaName = "tasks_BsToPhiGamma_MCTunesRun3ECM13p6TeV"
+eventsPerJob = 4000
+numberOfJobs = 500
+outLFNDirBase = "/store/group/dpg_trigger/comm_trigger/L1Trigger/OMTF/TrackingVertexing/"
+storage_element="T2_CH_CERN"
+outputDatasetTag = "BsToPhiGamma_14_0_15_patch1_02_10_2024"
+withPileUp = False
+withReco = True
+runLocal = False
+
+
+#########################################
+for aGenFragment in generator_fragments:
+
+    requestName = aGenFragment.split("00025")[0].rstrip("-")
+    requestName+="_"+era+"_"+outputDatasetTag
+    process = runCMSDriver(era, withPileUp, withReco, genFragmentsDirectory+aGenFragment)
+    dumpProcess(process, "PSet.py")
+
+    prepareCrabCfg(workAreaName, eventsPerJob, numberOfJobs,
+                  outLFNDirBase, storage_element, 
+                  requestName, outputDatasetTag)
+
+    if not runLocal:
+        os.system("crab submit -c crabTmp.py")
+        os.system("rm -f PSet.py* crabTmp.py*")      
+#######################################################
