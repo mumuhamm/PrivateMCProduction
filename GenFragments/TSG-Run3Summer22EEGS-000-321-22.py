6a10f95901a0c5a4e3c96a60f30af153e169a11f
Alibordi <alibordi.muhammad@cern.ch>
2024-10-02 15:43:49 +0200

    BsToJpsiEta and BsToPhi(KK)gamma is added

---
+++ GenFragments/TSG-Run3Summer22EEGS-000-321-22.py
@@ -0,0 +1,61 @@
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP5Settings_cfi import *
+from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
+
+_generator = cms.EDFilter(
+    "Pythia8GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(True),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    comEnergy = cms.double(13600.0),
+    ExternalDecays = cms.PSet(
+        EvtGen130 = cms.untracked.PSet(
+            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
+            particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_Bsmm.pdl'),
+            user_decay_file = cms.vstring('GeneratorInterface/EvtGenInterface/data/Bs_mumugamma.dec'),
+            list_forced_decays = cms.vstring('MyB_s0', 'Myanti-B_s0'),
+            operates_on_particles = cms.vint32(),
+            convertPythiaCodes = cms.untracked.bool(False),
+        ),
+        parameterSets = cms.vstring('EvtGen130')
+    ),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring(
+            "SoftQCD:nonDiffractive = on",
+            'PTFilter:filter = on', # this turn on the filter
+            'PTFilter:quarkToFilter = 5', # PDG id of q quark
+            'PTFilter:scaleToFilter = 1.0'),
+        parameterSets = cms.vstring(
+            'pythia8CommonSettings',
+            'pythia8CP5Settings',
+            'processParameters',
+        )
+    )
+)
+
+from GeneratorInterface.Core.ExternalGeneratorFilter import ExternalGeneratorFilter
+generator = ExternalGeneratorFilter(_generator)
+
+bfilter = cms.EDFilter(
+    "PythiaFilter",
+    MaxEta = cms.untracked.double(9999.),
+    MinEta = cms.untracked.double(-9999.),
+    ParticleID = cms.untracked.int32(531)
+)
+
+decayfilter = cms.EDFilter("PythiaAllDauVFilter",
+    moduleLabel = cms.untracked.InputTag("generator","unsmeared"),
+    verbose         = cms.untracked.int32(1),
+    NumberDaughters = cms.untracked.int32(3),
+    ParticleID      = cms.untracked.int32(531),
+    DaughterIDs     = cms.untracked.vint32(321, -321, 22),
+    MinPt           = cms.untracked.vdouble(3.5, 3.5, 2.50),
+    MinEta          = cms.untracked.vdouble(-2.5, -2.5, -3.0),
+    MaxEta          = cms.untracked.vdouble( 2.5,  2.5,  3.0)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*bfilter*decayfilter)
